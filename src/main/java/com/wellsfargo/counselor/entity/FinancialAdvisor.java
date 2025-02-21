4696b297570c263f68477e4ea3270b1f4066dde4
Komatireddy Santhoshreddy <santhoshreddyk555@gmail.com>
2025-02-20 22:04:50 -0500

    updating entities

--- src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -1,79 +1,44 @@
 package com.wellsfargo.counselor.entity;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.List;
 
 @Entity
 public class FinancialAdvisor {
-
     @Id
-    @GeneratedValue()
-    private long advisorId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @Column(nullable = false)
-    private String firstName;
-
-    @Column(nullable = false)
-    private String lastName;
-
-    @Column(nullable = false)
-    private String address;
-
-    @Column(nullable = false)
-    private String phone;
-
-    @Column(nullable = false)
+    private String name;
     private String email;
+    private String phoneNumber;
 
-    protected FinancialAdvisor() {
+    @OneToMany(mappedBy = "financialAdvisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
 
-    }
+    public FinancialAdvisor() {}
 
-    public FinancialAdvisor(String firstName, String lastName, String address, String phone, String email) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
+    public FinancialAdvisor(String name, String email, String phoneNumber) {
+        this.name = name;
         this.email = email;
+        this.phoneNumber = phoneNumber;
     }
 
-    public Long getAdvisorId() {
-        return advisorId;
+    public Long getId() {
+        return id;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public String getName() {
+        return name;
     }
 
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getEmail() {
@@ -82,5 +47,21 @@ public class FinancialAdvisor {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
     }
 }
