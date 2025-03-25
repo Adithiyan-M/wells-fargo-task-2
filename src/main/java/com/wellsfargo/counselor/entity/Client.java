8df633ca62aa19115750c35cf5b0c7fc38723df0
KazeAsh <pilgerashley@yahoo.com>
2025-03-25 16:03:23 +0900

    Made Client.java, Portfolio, and Security to all have @Entity had each Entity get their respected ids. Not setting since it is auto-generated. Created other necessary fields with their Column nullable = false, with the data type and set getters and setters for each field. Lastly updated Advisor and the rest to have @GeneratedValue(strategy = GenerationType.IDENTITY) for best practice.

--- src/main/java/com/wellsfargo/counselor/entity/Client.java
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -1,14 +1,15 @@
 package com.wellsfargo.counselor.entity;
 
-
 import jakarta.persistence.*;
 
+import java.io.Serializable;
+
 @Entity
-public class Advisor {
+public class Client implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long advisorId;
+    private long clientId;
 
     @Column(nullable = false)
     private String firstName;
@@ -25,11 +26,13 @@ public class Advisor {
     @Column(nullable = false)
     private String email;
 
-    protected Advisor() {
-
+    protected Client() {
+        this("", "", "", "", "");
     }
 
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
+
+    //create getter and setters for each field
+    public Client(String firstName, String lastName, String address, String phone, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.address = address;
@@ -37,47 +40,39 @@ public class Advisor {
         this.email = email;
     }
 
-    public Long getAdvisorId() {
-        return advisorId;
+    public long getClientId() {
+        return clientId;
     }
 
     public String getFirstName() {
         return firstName;
     }
-
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
-
     public String getLastName() {
         return lastName;
     }
-
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
     public String getAddress() {
         return address;
     }
-
     public void setAddress(String address) {
         this.address = address;
     }
-
     public String getPhone() {
         return phone;
     }
-
     public void setPhone(String phone) {
         this.phone = phone;
     }
-
     public String getEmail() {
         return email;
     }
-
     public void setEmail(String email) {
         this.email = email;
     }
+
 }
---
+++ src/main/java/com/wellsfargo/counselor/entity/Portfolio.java
@@ -0,0 +1,41 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+public class Portfolio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long portfolioId;
+
+    @Column(nullable = false)
+    @CreatedDate
+    private String create_date;
+
+    @Column(nullable = false)
+    @UpdateTimestamp
+    private String update_date;
+
+    //create getter and setters for each field
+    public long getId() {
+        return portfolioId;
+    }
+
+    public String getCreate_date() {
+        return create_date;
+    }
+
+    public void setCreate_date(String create_date) {
+        this.create_date = create_date;
+    }
+
+    public String getUpdate_date() {
+        return update_date;
+    }
+    public void setUpdate_date(String update_date) {
+        this.update_date = update_date;
+    }
+}
